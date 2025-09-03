243af158b2bcc302e84dbe762b3b00e177842137
emile <emilesavoie39@gmail.com>
2025-09-03 11:03:04 -0400

    lab poll

--- racecar_beacon/racecar_beacon/lab_poll_pos.py
+++ racecar_beacon/racecar_beacon/lab_poll_pos.py
@@ -14,16 +14,16 @@ from racecar_beacon.utils import yaw_from_quaternion
 class PositionPoller(Node):
     def __init__(self):
         super().__init__("position_poller")
-        # TODO: Add your subscription(s) here. Use the following syntax:
-        #   `self.subscription = self.create_subscription(<type>, <topic>, <self.callback>, 1)`
+        self.subscriptions = self.create_subscription(
+            Odometry,
+            "/odometry/filtered",
+            self.odom_callback,
+            10,
+        )
         self.get_logger().info(f"{self.get_name()} started.")
 
-    # TODO: Add your subscription callback(s) here. Use the following syntax:
-    #   ```python3
-    #    def <callback>(self, msg: <type>) -> None:
-    #       pass
-    #   ```
-
+    def odom_callback(self, msg: Odometry) -> None:
+        self.get_logger().info(f"Received odometry data: {msg}")
 
 def main(args=None):
     try:
