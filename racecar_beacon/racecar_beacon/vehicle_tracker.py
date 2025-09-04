1adb482434df181baf3bb045a192edcefd022d37
emile <emilesavoie39@gmail.com>
2025-09-04 13:54:49 -0400

    update

--- racecar_beacon/racecar_beacon/vehicle_tracker.py
+++ racecar_beacon/racecar_beacon/vehicle_tracker.py
@@ -7,17 +7,16 @@ PORT = 65431
 MAX_BYTES = 1024
 BIND_ADDR = ""
 
-# This specifies the packet structure for python
-STRUCT_FMT  = "!4sHBBfffI"
+# This specifies the packet structure
+STRUCT_FMT  = "!4sHfffI"
 """
 Struct FMT
 4s -> 4 byte string (magic = "POS1")
 H  -> uint16 (vehicle_id)
-B  -> uint8 (flags)
-B  -> uint8 (flags)
 fff -> float32 (x, y, yaw)
 I  -> uint32 (Sequence number)
 """
+
 STRUCT_SIZE = calcsize(STRUCT_FMT)
 
 def main():
@@ -32,8 +31,8 @@ def main():
             ts = datetime.now().strftime("%H:%M:%S")
 
             if len(data) >= STRUCT_SIZE:
-                _magic, _veh_id, _flags, _res, x, y, _yaw, _seq = unpack(STRUCT_FMT, data[:STRUCT_SIZE])
-                print(f"{ts} {src_ip}:{src_port}  x={x:.3f}  y={y:.3f}")
+                _magic, _veh_id, _res, x, y, _yaw, _seq = unpack(STRUCT_FMT, data[:STRUCT_SIZE])
+                print(f"{ts} {src_ip}:{src_port}  x={x:.3f}  y={y:.3f}, seq={_seq}  veh_id={_veh_id}  {len(data)}B")
             else:
                 print(f"{ts} {src_ip}:{src_port}  {len(data)}B (too short)")
 
