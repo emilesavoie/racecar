97e39d4bd4f260762a078e68891f765ee64c360e
Elisabeth0101 <sime2005@usherbrooke.ca>
2025-09-05 18:05:53 -0400

    Working RemoteClient

--- racecar_beacon/racecar_beacon/remote_client.py
+++ racecar_beacon/racecar_beacon/remote_client.py
@@ -3,16 +3,35 @@
 import socket
 from struct import unpack
 
-HOST = str("172.20.10.2")
+HOST = str("172.20.10.4")
 PORT = int(65432)
+MAX_BYTES = 1024
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, server!").encode('utf-8')
-    data = s.recv(1024)
-    print(f"Received {data!r}")
-    s.close()
+    while True:
+        cmd = input("Enter command (RPOS/OBSF/RBID): ").strip().upper()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            s.connect((HOST, PORT))
+            s.sendall(cmd.encode('ascii'))  # send 4 ASCII characters
+            while True:
+                data, addr = s.recvfrom(MAX_BYTES)
+                if data is not None:
+                    if cmd == "RPOS":
+                        msg = unpack('fff',data)
+                    elif cmd == "OBSF":
+                        msg = unpack('I',data)[0]
+                    elif cmd == "RBID":
+                        msg = unpack('I',data)[0]
+                    print(msg)
+                    break
+            
+        except KeyboardInterrupt:
+            pass
+        finally:
+            s.close()
+
 
 if __name__ == "__main__":
     main()
